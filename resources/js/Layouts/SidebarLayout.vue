e457da11c08151d07fdcbdc4aab01ea88468e2b5
MH Munna <Monirhasanmunna1@gmail.com>
2024-05-29 01:07:55 +0600

    design some change

--- resources/js/Layouts/SidebarLayout.vue
+++ resources/js/Layouts/SidebarLayout.vue
@@ -5,11 +5,11 @@ import MenuArrowIcon from '@/Components/Icons/MenuArrowIcon.vue';
 </script>
 
 <template>
-    <div id="sidebar" class="sidebar w-[0px] sm:min-w-[250px] bg-[#f5f7f9] shadow-xl shadow-blue-300 dark:bg-gray-800 fixed h-full overflow-hidden overflow-y-auto">
+    <div id="sidebar" class="sidebar w-[0px] sm:min-w-[250px]  bg-[#f5f7f9] dark:bg-gray-800 fixed h-full overflow-hidden overflow-y-auto">
         <nav class="hs-accordion-group py-6 px-[8px] w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
             <ul class="space-y-2">
                 <li>
-                    <a class="flex items-center gap-x-2 py-[9px] px-2.5 bg-[#6FD943] text-[15px] text-white  rounded-lg hover:bg-[#6FD943] hover:text-white hover:font-semibold dark:bg-neutral-700 dark:text-white" href="#">
+                    <a class="flex items-center gap-x-2 py-[9px] px-2.5 text-[15px] text-gray-700 duration-200 rounded-lg hover:bg-[#6FD943] hover:text-white hover:font-semibold dark:bg-neutral-700 dark:text-white" href="#">
                     <DashboardIcon class="menu-icon" />
                     Dashboard
                     </a>
@@ -20,6 +20,7 @@ import MenuArrowIcon from '@/Components/Icons/MenuArrowIcon.vue';
                     <button type="button" class="menu hs-accordion-toggle">
                     <svg class="flex-shrink-0 mt-0.5 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="18" cy="15" r="3"/><circle cx="9" cy="7" r="4"/><path d="M10 15H6a4 4 0 0 0-4 4v2"/><path d="m21.7 16.4-.9-.3"/><path d="m15.2 13.9-.9-.3"/><path d="m16.6 18.7.3-.9"/><path d="m19.1 12.2.3-.9"/><path d="m19.6 18.7-.4-1"/><path d="m16.8 12.3-.4-1"/><path d="m14.3 16.6 1-.4"/><path d="m20.7 13.8 1-.4"/></svg>
                     Account
+
                     <MenuArrowIcon />
                     </button>
 
