32718a4904aa5ed07166538996b0072a4fc2a87e
MH Munna <Monirhasanmunna1@gmail.com>
2024-05-28 17:32:42 +0600

    init

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -4,7 +4,9 @@ import { Head, Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    
+    <div class="flex justify-center items-center w-full h-screen">
+        <h1 class="text-3xl text-green-600">Home Page</h1>
+    </div>
 </template>
 
 <style scoped>
