973c18109d66a53fb30d19958d6561cfe915176e
MH Munna <Monirhasanmunna1@gmail.com>
2024-05-29 20:19:35 +0600

    breadcrumb dynamic done

---
+++ resources/js/Components/Backend/BreadcrumbComponent.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+
+   const props = defineProps({
+        breadcrumb : Object
+    });
+    
+</script>
+
+<template>
+    <div class="flex items-center justify-between w-full pb-[40px] pt-[30px] px-1">
+
+           <h1 class="text-xl font-semibold text-gray-500 dark:text-white">{{ breadcrumb.title }}</h1>
+
+            <ol class="flex items-center whitespace-nowrap justify-end">
+            <li class="inline-flex items-center">
+                <Link :href="route('dashboard')" class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500" href="#">
+                <svg class="flex-shrink-0 me-3 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                    <polyline points="9 22 9 12 15 12 15 22"></polyline>
+                </svg>
+                Home
+                </Link>
+                <svg class="flex-shrink-0 mx-2 overflow-visible size-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="m9 18 6-6-6-6"></path>
+                </svg>
+            </li>
+
+            <li v-for="(item, index) in breadcrumb.items" :key="index" class="inline-flex items-center">
+                <Link :href="item.link" :class="{'text-green-600': item.active == true }" class="flex items-center text-sm text-gray-500 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500" href="#">
+                {{ item.name }}
+                <svg v-if="breadcrumb.items.length - 1 !== index" class="flex-shrink-0 mx-2 overflow-visible size-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m9 18 6-6-6-6"></path>
+                </svg>
+                </Link>
+            </li>
+        </ol>
+    </div>
+</template>
--- resources/js/Pages/Category/Index.vue
+++ resources/js/Pages/Category/Index.vue
@@ -1,11 +1,26 @@
 <script setup>
 import Box from '@/Components/Box.vue';
 import MasterLayout from '@/Layouts/MasterLayout.vue';
+import BreadcrumbComponent from '@/Components/Backend/BreadcrumbComponent.vue';
+
+const breadcrumb = {
+    title : 'Category',
+
+    items : [
+        {
+            name: 'Category',
+            link: '/category',
+            active: true
+        }
+    ]
+}
+
 </script>
 
 <template>
     <Head title="Category" />
     <MasterLayout>
+        <BreadcrumbComponent :breadcrumb="breadcrumb" />
         <Box>
             <h3>Categeory</h3>
         </Box>
